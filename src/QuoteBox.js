d8c3731fe4d38722690a256fddfe3007586e0115
radhamesc-capellan <85122601+radhamesc-capellan@users.noreply.github.com>
2021-06-28 18:07:10 -0500

    Update QuoteBox.js

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -13,7 +13,7 @@ const QuoteBox = () => {
 
     const getQuote = () => {
         
-        let url = `https://github.com/radhamesc-capellan/Quote_Machine/blob/master/src/quotes.json`;
+        let url = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
         fetch(url)
         .then(res => res.json())
         .then(data => {
